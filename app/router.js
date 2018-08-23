806db678ecf7cbecdf0c6f2312d486b48939e093
rquast <r.quast@uq.edu.au>
2018-08-23 15:58:28 +1000

    added check for loading of foundation

--- app/router.js
+++ app/router.js
@@ -6,10 +6,14 @@ import $ from 'jquery';
 const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL,
+  foundationLoaded: false,
   didTransition: function() {
     scheduleOnce('afterRender', this, () => {
       //Enable Foundation
-      $(document).foundation();
+      if (!this.foundationLoaded) {
+        $(document).foundation();
+        this.foundationLoaded = true;
+      }
       //Scroll to top
       window.scrollTo(0, 0);
     });
