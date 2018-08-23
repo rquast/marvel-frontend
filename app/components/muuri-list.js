4cea50f77ea43e6f03e95d8345735c8d324dee40
rquast <r.quast@uq.edu.au>
2018-08-23 16:23:39 +1000

    added character select action

--- app/components/muuri-list.js
+++ app/components/muuri-list.js
@@ -3,5 +3,10 @@ import Component from '@ember/component';
 export default Component.extend({
   didInsertElement(){
     var muuri = new Muuri('.muuri-grid');
+  },
+  actions: {
+    selectCharacter(character) {
+      alert(character.id);
+    }
   }
 });
