7e3aa67e15ec4a213bb311327f908c5d7b24285c
rquast <r.quast@uq.edu.au>
2018-08-23 15:46:58 +1000

    fixed equalize for scaling of sidebar

--- app/router.js
+++ app/router.js
@@ -1,9 +1,19 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import { scheduleOnce }  from '@ember/runloop';
+import $ from 'jquery';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+  didTransition: function() {
+    scheduleOnce('afterRender', this, () => {
+      //Enable Foundation
+      $(document).foundation();
+      //Scroll to top
+      window.scrollTo(0, 0);
+    });
+  }
 });
 
 Router.map(function() {
