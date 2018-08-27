60fd50229435a185383b05ed9c278d9766fa618b
rquast <r.quast@uq.edu.au>
2018-08-28 09:18:36 +1000

    added notification for username or password not valid

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,13 +3,16 @@ import { inject } from '@ember/service';
 
 export default Controller.extend({
   session: inject('session'),
-
   actions: {
     authenticate: function() {
       const credentials = this.getProperties('username', 'password');
       const authenticator = 'authenticator:jwt';
+      this.set('isLoginIn', true);
       this.get('session').authenticate(authenticator, credentials).then(() => {
         this.transitionToRoute('characters');
+      }).catch((err) => {
+        this.set('fieldErrors', err);
+        this.set('isLoginIn', false);
       });
     }
   }
