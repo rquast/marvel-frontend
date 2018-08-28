46a317603d3536c6ab9c4a9d04eb8dc256cf03ae
rquast <r.quast@uq.edu.au>
2018-08-28 10:21:00 +1000

    redirect after logout

--- app/components/login-component.js
+++ app/components/login-component.js
@@ -11,6 +11,8 @@ export default Component.extend({
   actions: {
     invalidateSession: function() {
       this.get('session').invalidate();
+      // hack because it's easier. :)
+      window.location.href = '/login';
     }
   }
 });
